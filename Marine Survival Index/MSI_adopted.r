d575e0697e79ef429e5586f32009f7a94fda545d
ErikSuring <erik.suring@oregonstate.edu>
2025-12-12 13:23:54 -0800

    Clean up evaluation code to create adopted MSI

--- Marine Survival Index/MSI_adopted.r
+++ Marine Survival Index/MSI_adopted.r
@@ -77,169 +77,17 @@ MSI_Three <- MSI_Three_Weight%>%
 MSI_Three$MSAdjThree <- (exp(MSI_Three$MSAdjThree)/(exp(MSI_Three$MSAdjThree)+1))
 MSI_ESU <- left_join(MSI_ESU, MSI_Three)
 
-print(ggplot(data=MSI_ESU, aes(x=ReturnYear, y=MSAdjMARSS)) +
-        xlab("Return Year") +
-        ylab("Adj Marine Survival") +
-        ggtitle(paste("MSI Alternatives")) +
-        geom_line(color="orange",linewidth=1.5) + 
-        geom_point(color="orange", size = 2) +
-        #geom_line(aes(y=MSAdjOG), col='black', linewidth=1.5) +
-        #geom_point(aes(y=MSAdjOG), col='black', size = 2) +
-        geom_line(aes(y=MSAdjThree), col='blue', linewidth=1.5) +
-        geom_point(aes(y=MSAdjThree), col='blue', size = 2) +
-        theme_bw())
-
-
-#Progressive cross-validation
-#Run the MARSS alternative with 3 to 8 years of MS LCM data removed
-for (i in 3:8){
-  #Remove WF Smith and Winchester from the dataset
-  MSI_data_cv <- MSI_data %>%
-    filter(!((between(ReturnYear, (2025-i), 2024)) & (Site == "West Fork Smith" | Site == "Winchester Creek")))
-  
-  MSI_table_cv<-MSI_data_cv%>%
-    pivot_wider(names_from = Site,values_from = MSAdj,id_cols=ReturnYear)
-  
-  #Alternative One: use MARSS to interpolate missing values
-  marss_mat<-MSI_table_cv%>%
-    dplyr::select(-c(ReturnYear))%>%
-    as.matrix()%>%
-    t()
-  
-  model=list(Q= "equalvarcov")
-  fit=MARSS(marss_mat, model=model, control=list(maxit=1000))
-  
-  fitted<-t(fit$states)
-  colnames(fitted)<-gsub("X.","mle_",colnames(fitted))
-  
-  MSI_fitted<-MSI_table_cv%>%
-    bind_cols(fitted)%>%
-    pivot_longer(cols=c(everything(), -ReturnYear),names_to = "Site")%>%
-    mutate(type=ifelse(grepl("mle_",Site),"mle","obs"))%>%
-    mutate(Site=gsub("mle_","",Site))%>%
-    filter(type=="mle")%>%
-    group_by(ReturnYear,Site)%>%
-    summarise(MSAdj=mean(value))
-  
-  
-  
-  MSI_fitted <- MSI_fitted%>%
-    group_by(ReturnYear)%>%
-    summarise(MSAdjMARSScv=mean(MSAdj))
-  MSI_fitted$MSAdjMARSScv <- (exp(MSI_fitted$MSAdjMARSScv)/(exp(MSI_fitted$MSAdjMARSScv)+1))
-  colnames(MSI_fitted)[2] <- paste0("MSAdjMARSSm", i)
-  nrow(MSI_fitted)
-  MSI_fitted[(nrow(MSI_fitted)-(i-1)):nrow(MSI_fitted),]
-  
-  MSI_ESU <- left_join(MSI_ESU, MSI_fitted[(nrow(MSI_fitted)-(i-1)):nrow(MSI_fitted),])
-  
-  MSline <- sym(paste0("MSAdjMARSSm", i))
-  print(ggplot(data=MSI_ESU, aes(x=ReturnYear, y=MSAdjCurrent)) +
-    xlab("Return Year") +
-    ggtitle(paste("OCV minus", i)) +
-    geom_line(color="#009E73",linewidth=1.5) + 
-    geom_point(color="#009E73", size = 2) +
-    geom_line(aes(y=!!MSline), col='#D55E00', linewidth=1.5) +
-    geom_point(aes(y=!!MSline), col='#D55E00', size = 2) +
-    geom_line(aes(y=MSAdjThree), col='#0072B2', linewidth=1.5) +
-    geom_point(aes(y=MSAdjThree), col='#0072B2', size = 2) +
-    geom_line(aes(y=MSAdjOG), col='#000000', linewidth=1.5) +
-    geom_point(aes(y=MSAdjOG), col='#000000', size = 2) +
-    theme_bw())
-  
-}
-
-
-#Create evaluation table
-MSI_eval_columns <- c("Years_removed", "MARSS_MAE", "Three_site_MAE", "MARSS_MAPE", "Three_site_MAPE")
-MSI_eval <- data.frame(matrix(nrow = 0, ncol = length(MSI_eval_columns)))
-for (i in 3:8){
-  startrow <- nrow(MSI_ESU)+1-i
-  endrow <- nrow(MSI_ESU)
-  MARSScol <- 3+i
-  MSI_eval <- rbind(MSI_eval,c(i, 
-                    mae(MSI_ESU$MSAdjCurrent[startrow:endrow], pull(MSI_ESU[startrow:endrow,MARSScol])), 
-                    mae(MSI_ESU$MSAdjCurrent[startrow:endrow], MSI_ESU$MSAdjThree[startrow:endrow]), 
-                    mape(MSI_ESU$MSAdjCurrent[startrow:endrow], pull(MSI_ESU[startrow:endrow,MARSScol])), 
-                    mape(MSI_ESU$MSAdjCurrent[startrow:endrow], MSI_ESU$MSAdjThree[startrow:endrow])))
-}
-colnames(MSI_eval) <- MSI_eval_columns
-
-
-print(ggplot(data=MSI_eval, aes(x=Years_removed, y=MARSS_MAE)) +
-        xlab("Years Removed") +
-        ggtitle("MAE") +
-        geom_line(color="black",linewidth=1.5) + 
-        geom_point(color="black", size = 2) +
-        geom_line(aes(y=Three_site_MAE), col='orange', linewidth=1.5) +
-        geom_point(aes(y=Three_site_MAE), col='orange', size = 2) +
-        theme_bw())
-
-print(ggplot(data=MSI_eval, aes(x=Years_removed, y=MARSS_MAPE)) +
-        xlab("Years Removed") +
-        ggtitle("MAPE") +
-        geom_line(color="black",linewidth=1.5) + 
-        geom_point(color="black", size = 2) +
-        geom_line(aes(y=Three_site_MAPE), col='orange', linewidth=1.5) +
-        geom_point(aes(y=Three_site_MAPE), col='orange', size = 2) +
-        theme_bw())
-
-#Print the evaluation table
-MSI_eval_table <- gt(MSI_eval) %>%
-  tab_header(
-    title = "Marine survival index alternative evaluation",
-  ) %>%
-  fmt_number(
-    columns = c(MARSS_MAE, Three_site_MAE, MARSS_MAPE, Three_site_MAPE),
-    decimals = 3
-  ) %>%
-  cols_label(
-    Years_removed = "Years Removed",
-    MARSS_MAE = "MAE MARSS",
-    Three_site_MAE = "MAE Three Sites",
-    MARSS_MAPE = "MAPE MARSS",
-    Three_site_MAPE = "MAPE Three Sites"
-  ) %>%
-  tab_style(
-    style = list(
-      cell_text(weight = "bold"),
-      cell_text(align = "center")
-    ),
-    locations = cells_column_labels(everything())
-  )%>%
-  tab_style(
-    style = list(
-      cell_text(align = "center")
-    ),
-    locations = cells_body()
-  ) %>%
-  tab_options(
-    table.font.size = "12px"
-  )
-
-print(MSI_eval_table)
-gtsave(data = MSI_eval_table,
-  filename = "MSI_eval_table.png",
-  expand = 5,  # Optional: adds padding around the table
-  vwidth = 7 * 96,  # Convert inches to pixels (assuming 96 DPI)
-  vheight = 4 * 96
-)
-
-
-
-#Print graph of MSIs used in Forecast Evaluations
+#Print graph of MSI input to be used in the marine survival forecast
 MSI_graph <- MSI_ESU
-MSI_graph$MSAdjThree[1:21] <- NA
-MSI_graph$MSAdjCurrent[1:18] <- NA
 MSI_graph_long <- MSI_graph %>%
-  select(ReturnYear, MSAdjCurrent, MSAdjMARSSm5, MSAdjThree, MSAdjOG)
+  select(ReturnYear, MSAdjMARSS, MSAdjThree, MSAdjOG)
 MSI_graph_long <- pivot_longer(MSI_graph_long,
-                              cols = c(MSAdjCurrent, MSAdjMARSSm5, MSAdjThree, MSAdjOG),
+                              cols = c(MSAdjMARSS, MSAdjThree, MSAdjOG),
                               names_to = "Metric",
                               values_to = "Value")
 MSI_graph_long$Metric <- factor(MSI_graph_long$Metric,
-                                levels = c("MSAdjOG", "MSAdjCurrent", "MSAdjThree", "MSAdjMARSSm5"),
-                                labels = c("Six site MSI (2013)", "Five site MSI (2017)", "Three site MSI", "Interpolated MSI"))
+                                levels = c("MSAdjOG", "MSAdjThree", "MSAdjMARSS"),
+                                labels = c("Six site MSI (2013)", "Three site MSI", "Interpolated MSI (2025)"))
 
 MSI_alt_plot <- ggplot(data = MSI_graph_long, aes(x = ReturnYear, y = Value, color = Metric)) +
   geom_line(linewidth = 1.5) +
@@ -248,9 +96,8 @@ MSI_alt_plot <- ggplot(data = MSI_graph_long, aes(x = ReturnYear, y = Value, col
     name = NULL,
     values = c(
       "Six site MSI (2013)" = "#000000",
-      "Five site MSI (2017)" = "#009E73",
       "Three site MSI" = "#0072B2",
-      "Interpolated MSI" = "#D55E00"
+      "Interpolated MSI (2025)" = "#D55E00"
     )
   ) +
   scale_y_continuous(
@@ -274,27 +121,8 @@ ggsave("MSI_plot.png", plot = MSI_alt_plot +
          coord_cartesian(clip = "off"),
        width = 7, height = 4, units = "in", dpi = 300)
 
-#Other comparisons of the MSI alternatives
-cor(MSI_ESU$MSAdjOG[1:19], MSI_ESU$MSAdjThree[1:19])
-cor(MSI_ESU$MSAdjOG[1:19], MSI_ESU$MSAdjMARSS[1:19])
-cor(MSI_ESU$MSAdjCurrent, MSI_ESU$MSAdjThree)
-cor(MSI_ESU$MSAdjCurrent, MSI_ESU$MSAdjMARSS)
-cor(MSI_ESU$MSAdjCurrent[22:26], MSI_ESU$MSAdjThree[22:26])
-cor(MSI_ESU$MSAdjCurrent[22:26], MSI_ESU$MSAdjMARSSm5[22:26])
-
-#write_csv(MSI_graph, "MSI_graph.csv")
-
-#Create the MSI inputs to be used in the marine survival forecast
+#Create the MSI input to be used in the marine survival forecast
 MSI_results <- MSI_ESU %>%
-  select(ReturnYear, MSAdjCurrent, MSAdjOG, MSAdjMARSSm5, MSAdjThree)
-#Use the original MSI up to 2017
-  MSI_results$MSAdjCurrent[1:19] <- MSI_results$MSAdjOG[1:19]
-  MSI_results$MSAdjThree[1:19] <- MSI_results$MSAdjOG[1:19]
-  MSI_results$MSAdjMARSSm5[1:19] <- MSI_results$MSAdjOG[1:19]
-#Use the current MSI up to 2019, MSI alternatives are used after this
-  MSI_results$MSAdjOG[20:21] <- MSI_results$MSAdjCurrent[20:21]
-  colnames(MSI_results)[3] <- "MSAdjFixed"
-  MSI_results$MSAdjThree[20:21] <- MSI_results$MSAdjCurrent[20:21]
-  MSI_results$MSAdjMARSSm5[20:21] <- MSI_results$MSAdjCurrent[20:21]
+  select(ReturnYear, MSAdjMARSS)
 
 write_csv(MSI_results, "MSI_results.csv")  
